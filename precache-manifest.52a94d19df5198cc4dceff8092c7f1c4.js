11c1d3f25a7279c4674fd0de4345a4c3800f3d55
Thomas Carli <thomas.carli@gmail.com>
2020-10-23 09:47:26 +0200

    Updates

---
+++ precache-manifest.52a94d19df5198cc4dceff8092c7f1c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fb896a6bca1ce4949fc66bf0bd5d43",
+    "url": "/flightplot/index.html"
+  },
+  {
+    "revision": "a0802d7b3ec89c24d51c",
+    "url": "/flightplot/static/css/2.9015c732.chunk.css"
+  },
+  {
+    "revision": "de3574179646af41647c",
+    "url": "/flightplot/static/css/main.168b535a.chunk.css"
+  },
+  {
+    "revision": "a0802d7b3ec89c24d51c",
+    "url": "/flightplot/static/js/2.db3147be.chunk.js"
+  },
+  {
+    "revision": "932e70052e06440bc7deb8ba9e4236cb",
+    "url": "/flightplot/static/js/2.db3147be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de3574179646af41647c",
+    "url": "/flightplot/static/js/main.c8df50bf.chunk.js"
+  },
+  {
+    "revision": "2de71bc8cab1bdb086b3",
+    "url": "/flightplot/static/js/runtime-main.3b055fa1.js"
+  }
+]);

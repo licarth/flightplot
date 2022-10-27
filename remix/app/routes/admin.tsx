387af626a09ac2053a9e09af5e74d27ecdf8b9e1
Thomas Carli <thomascarli@gmail.com>
2022-10-27 16:52:07 +0200

    log memoizee stats

--- remix/app/routes/admin.tsx
+++ remix/app/routes/admin.tsx
@@ -1,13 +1,19 @@
 import type { LoaderFunction } from 'remix';
-import { redirect } from 'remix';
+import { redirect, useLoaderData } from 'remix';
 import styled from 'styled-components';
 import { requireUserId } from '~/utils/session.server';
 
-export default () => (
-    <AppContainer id="app">
-        <h1>This is an admin page</h1>
-    </AppContainer>
-);
+export default () => {
+    const { memoizee } = useLoaderData();
+
+    return (
+        <AppContainer id="app">
+            <h1>This is an admin page</h1>
+            <h2>Memoizee statistics</h2>
+            <p>{memoizee.logStats}</p>
+        </AppContainer>
+    );
+};
 const AppContainer = styled.div`
     height: 100%;
 `;
@@ -15,8 +21,18 @@ const AppContainer = styled.div`
 export const loader: LoaderFunction = async ({ request }) => {
     const user = await requireUserId(request);
     if (user && user.email === 'thomascarli@gmail.com') {
-        return null;
+        return computeAdminStats();
     } else {
         return redirect('/');
     }
+};
+
+const computeAdminStats = () => {
+    const memProfile = require('memoizee/profile');
+    const logStats = memProfile.log();
+    return {
+        memoizee: {
+            logStats,
+        },
+    };
 };

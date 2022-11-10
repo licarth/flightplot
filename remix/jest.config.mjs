50bedac9dacd44ba1c476ed3d83ac984898b7c29
Thomas Carli <thomascarli@gmail.com>
2022-11-10 17:21:03 +0100

    fix tests

--- remix/jest.config.mjs
+++ remix/jest.config.mjs
@@ -7,7 +7,7 @@ const config = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testMatch: ['**/?(*.)+(spec|test).(js|ts)?(x)'],
     transformIgnorePatterns: [
-        '/node_modules/(?!@firebase|firebase|node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill)',
+        '/node_modules/(?!@firebase|firebase|node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill|react-leaflet|@react-leaflet/core|react-notion-x|notion-utils|p-queue|p-timeout|is-url-superb|mem|mimic-fn|normalize-url|@marfle)',
     ],
     transform: {
         '^.+\\.js$': ['esbuild-jest'],
@@ -21,6 +21,6 @@ const config = {
             },
         ],
     },
-    moduleNameMapper: hq.get('jest'),
+    moduleNameMapper: { ...hq.get('jest'), '^.+\\.(css|less)$': '<rootDir>/CSSStub.js' },
 };
 export default config;
--- remix/jest.setup.js
+++ remix/jest.setup.js
@@ -1,3 +1,4 @@
 import '@testing-library/jest-dom/extend-expect';
 import * as React from 'react';
 window.React = React;
+global.ResizeObserver = require('resize-observer-polyfill');

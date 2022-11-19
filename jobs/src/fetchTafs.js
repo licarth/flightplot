abd519f6e3f0ea13aa4a7e300b733da273107251
Thomas Carli <thomascarli@gmail.com>
2022-11-19 16:20:09 +0100

    fix jobs deployment

--- jobs/src/fetchTafs.js
+++ jobs/src/fetchTafs.js
@@ -1,12 +1,19 @@
 import * as dotenv from "dotenv";
 import { getFirestore } from "firebase-admin/firestore";
 import fetch from "node-fetch";
-import { getAllFrenchAirports } from "./getAllFrenchAirports.js";
+import { AiracData } from "ts-aerodata-france";
+import dataJson from "ts-aerodata-france/build/jsonData/2022-11-03.json" assert { type: "json" };
 
 dotenv.config();
 const TAF_API_URL = "https://api.checkwx.com/taf";
 
 const checkwxApiToken = process.env.CHECKWX_API_TOKEN || "";
+async function getAllFrenchAirports() {
+  return (await AiracData.loadCycle(dataJson)).aerodromes
+    .filter(({ icaoCode }) => isNaN(Number(icaoCode)))
+    .map(({ icaoCode }) => `${icaoCode}`)
+    .join(",");
+}
 
 export const fetchTafs = async () => {
   const icaoCodes = await getAllFrenchAirports();
--- jobs/src/now.js
+++ jobs/src/now.js
@@ -1,4 +1,5 @@
 import { fetchMetars } from "./fetchMetarsNoaa.js";
+import { fetchTafs } from "./fetchTafs";
 import { initializeApp } from "./initializeApp";
 
 initializeApp();

0a0ab114206d034cfc0614da14fc63a014088f6d
Thomas Carli <thomascarli@gmail.com>
2023-09-21 09:07:19 +0200

    fix metars job

--- jobs/src/index.js
+++ jobs/src/index.js
@@ -1,8 +1,9 @@
-import { fetchMetars } from "./fetchMetarsNoaa.js";
 import cron from "node-cron";
+import { fetchMetars } from "./fetchMetarsNoaa.js";
 
 fetchMetars();
 
 cron.schedule("0,30 * * * *", () => {
   fetchMetars();
+  fetch("https://hc-ping.com/616d2f87-d19e-4fe6-bc39-9227d6e7c245");
 });
